b7f2b4248aa5cfd0971daaba262dd8190ed44e04
ErV2i <148342940+ErV2i@users.noreply.github.com>
2024-05-16 16:45:03 +0200

    déplacement + plateforme

---
+++ Assets/Personnage/DeplacementPersonnage.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class DeplacementPersonnage : MonoBehaviour
+{
+    public float vitesseDeplacement = 5f;
+    public int jumpPower = 10;
+    bool grounded;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float deplacementHorizontal = Input.GetAxis("Horizontal");
+        Vector3 deplacement = new Vector3(deplacementHorizontal, 0f, 0f) * vitesseDeplacement * Time.deltaTime;
+        transform.Translate(deplacement);
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            Sauter();
+        }
+    }
+
+    void Sauter()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            grounded = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Jump"))
+        {
+            Sauter();
+        }
+    }
+}
